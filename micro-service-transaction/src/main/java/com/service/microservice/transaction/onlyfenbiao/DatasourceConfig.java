12132f2121f334906128cafe774d9c9bf1e5aed9
u-simon <u_simon@163.com>
2019-08-19 16:19:49 +0800

    fix

--- micro-service-transaction/src/main/java/com/service/microservice/transaction/onlyfenbiao/DatasourceConfig.java
+++ micro-service-transaction/src/main/java/com/service/microservice/transaction/onlyfenbiao/DatasourceConfig.java
@@ -51,11 +51,9 @@ public class DatasourceConfig {
 		Map<String, DataSource> dataSourceMap = Maps.newHashMapWithExpectedSize(2);
 		dataSourceMap.put(this.databaseName, createDataSource(databaseName));
 		// 设置默认db为ds_0，也就是为那些没有配置分库分表策略的指定的默认库
-
 		// 如果只有一个库，也就是不需要分库的话，map里只放一个映射就行了，只有一个库时不需要指定默认库，
-
 		// 但2个及以上时必须指定默认库，否则那些没有配置策略的表将无法操作数据
-		DataSourceRule rule = new DataSourceRule(dataSourceMap, "ds_0");
+		DataSourceRule rule = new DataSourceRule(dataSourceMap, databaseName);
 		// 2.设置分表映射，将t_order_0和t_order_1两个实际的表映射到t_order逻辑表
 		TableRule tableRule = TableRule.builder("user")
 				.actualTables(Arrays.asList("user_0", "user_1", "user_2", "user_3"))
