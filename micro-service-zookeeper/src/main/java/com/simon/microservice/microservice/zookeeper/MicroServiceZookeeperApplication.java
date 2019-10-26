404c9ec6ec87531fdb45e6c4bfa5bfadf9889352
u-simon <u_simon@163.com>
2019-10-26 14:09:35 +0800

    fix

--- micro-service-zookeeper/src/main/java/com/simon/microservice/microservice/zookeeper/MicroServiceZookeeperApplication.java
+++ micro-service-zookeeper/src/main/java/com/simon/microservice/microservice/zookeeper/MicroServiceZookeeperApplication.java
@@ -2,8 +2,12 @@ package com.simon.microservice.microservice.zookeeper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+import java.time.LocalTime;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy(exposeProxy = true)
 public class MicroServiceZookeeperApplication {
 
 	public static void main(String[] args) {
