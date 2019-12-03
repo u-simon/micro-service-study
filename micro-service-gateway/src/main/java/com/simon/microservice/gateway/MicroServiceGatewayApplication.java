507cef5d2ed8449e570347531e7f5cb44d3b4f71
u-simon <u_simon@163.com>
2019-12-03 14:07:31 +0800

    gateway

---
+++ micro-service-gateway/src/main/java/com/simon/microservice/gateway/MicroServiceGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.simon.microservice.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceGatewayApplication.class, args);
+	}
+
+}
---
+++ micro-service-gateway/src/test/java/com/simon/microservice/gateway/MicroServiceGatewayApplicationTests.java
@@ -0,0 +1,12 @@
+package com.simon.microservice.gateway;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {}
+
+}
