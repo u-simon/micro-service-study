15aeae36965f6b26716935b35e612f17721871a9
u-simon <u_simon@163.com>
2020-01-13 19:27:05 +0800

    fix

--- micro-service-consumer/src/main/java/com/simon/microservice/consumer/MicroServiceConsumerApplication.java
+++ micro-service-consumer/src/main/java/com/simon/microservice/consumer/MicroServiceConsumerApplication.java
@@ -1,10 +1,19 @@
 package com.simon.microservice.consumer;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.DiscoveryClient;
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -14,4 +23,22 @@ public class MicroServiceConsumerApplication {
 		SpringApplication.run(MicroServiceConsumerApplication.class, args);
 	}
 
+	@Resource(name = "eurekaClient")
+	EurekaClient eurekaClient;
+
+	public String serviceUrl() {
+		InstanceInfo stores = eurekaClient.getNextServerFromEureka("STORES", false);
+		return stores.getHomePageUrl();
+	}
+
+
+	DiscoveryClient discoveryClient;
+
+	public String serviceUrl1(){
+		List<InstanceInfo> stores = discoveryClient.getInstancesById("stores");
+		if (stores != null && stores.size() > 0){
+			return stores.get(0).getIPAddr();
+		}
+		return null;
+	}
 }
