f288fcdd22c7e5235b79678e7b4257a1a137347c
u-simon <u_simon@163.com>
2020-04-27 15:52:30 +0800

    git config

--- micro-service-zookeeper/src/test/java/com/simon/microservice/microservice/zookeeper/MicroServiceZookeeperApplicationTests.java
+++ micro-service-zookeeper/src/test/java/com/simon/microservice/microservice/zookeeper/MicroServiceZookeeperApplicationTests.java
@@ -1,6 +1,6 @@
 package com.simon.microservice.microservice.zookeeper;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
